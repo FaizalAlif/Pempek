73d529bfe1f1af57df6c47b6d9da4216ecfac4b7
FaizalAlif <37656196+FaizalAlif@users.noreply.github.com>
2019-05-09 11:49:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pempek/Prevalent/Prevalent.java
@@ -0,0 +1,13 @@
+package com.example.pempek.Prevalent;
+
+import com.example.pempek.Model.Users;
+
+    public class Prevalent
+    {
+        public static Users currentOnlineUser;
+
+        public static final String UserEmailKey = "UserEmail";
+        public static final String UserPasswordKey = "UserPassword";
+    }
+
+
